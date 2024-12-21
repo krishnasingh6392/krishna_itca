34c9f5ae9474c37588f4fd4c1674f65bb1933b06
Krishna Nand <161636619+krishnasingh6392@users.noreply.github.com>
2024-12-21 07:49:56 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  fontFamily: {
+    // Add custom font here
+    roboto: ["Roboto", "sans-serif"],
+  },
+  plugins: [],
+};
